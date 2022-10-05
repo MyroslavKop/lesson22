98dce35dbb3e285e379e819c008bcbaea6c9db54
MiroslavKop <kopytovmiroslav@gmail.com>
2022-10-05 13:18:39 +0300

    homework 22

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import Title from "./components/Title";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isOpened, setOpened] = useState(false);
+    const handleClick = () => {
+        setOpened(prevValue => !prevValue);
+    };
+    if (isOpened) {
+        return (
+            <>
+                <Title name="Products" type="normal"/>
+                <button onClick={handleClick}>Close title</button>
+            </>
+        );
+    } else {
+        return <button onClick={handleClick}>Open Title</button>;
+    }
+
 }
 
 export default App;
---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types";
+
+const Title = ({name, type}) => {
+    return (
+        <h1 style={{fontWeight: type}}>{name}</h1>
+    );
+};
+
+Title.propTypes = {
+    name: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(["normal", "bold"])
+};
+
+export default Title;
